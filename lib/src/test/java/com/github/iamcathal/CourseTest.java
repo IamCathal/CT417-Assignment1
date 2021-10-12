a9569bbd979562e9695876a63f7451d90cd10ba0
Cathal <cathalsocallaghan@gmail.com>
2021-10-12 20:03:36 +0100

    new commit

--- lib/src/test/java/com/github/iamcathal/CourseTest.java
+++ lib/src/test/java/com/github/iamcathal/CourseTest.java
@@ -39,7 +39,6 @@ public class CourseTest {
                 new DateTime(),
                 new DateTime()
         );
-
         assertEquals(testCourse.getCourseName(), expectedCourseName);
     }
 
